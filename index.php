ae7c479e1a6b6ba2ae296605a0946eae110cb9ef
camilamguzman <>
2020-10-20 23:18:13 -0400

    Set up sidebar and add pages

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+
+<!--include head section and navbar-->
+<?php include 'templates/header.php'; ?>
+
+<p>This is the body</p>
+
+<?php include 'templates/footer.php'; ?>
+
+</html>
---
+++ templates/header.php
@@ -0,0 +1,35 @@
+<head>
+    <meta name="viewport" content="width=device-width initial-scale=1.0" />
+    <title>Camila Guzman</title>
+
+    <!--stylesheet-->
+    <link rel="stylesheet" href="styles/main.css" />
+
+    <!--icon kit-->
+    <script src="https://kit.fontawesome.com/bf0ea5bc59.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="grid">
+        <div class="sidebar">
+            <!--logo
+            <img src='./images/cg-logo.png' class="logo-img" />-->
+            <!--title: name-->
+            <h2 class="name">Camila Guzman</h2>
+
+            <p>Front-end developer and designer interested in web development and product design!</p>
+
+            <div class="social-links">
+                <span><i class="fab fa-github-square"></i> </span><a href="https://github.com/camilamguzman" style="margin-left: 8px">@camilamguzman on GitHub</a><br>
+                <span><i class="fab fa-linkedin"></i></span><a class="linkedin" href="https://www.linkedin.com/in/camila-m-guzman/" style="margin-left: 12px">camila-m-guzman on LinkedIn</a>
+            </div>
+            <!--nav-->
+            <div class="nav-list">
+                <a href="./portfolio.php"> <span>Portfolio</span></a>
+                <a href="./about.php"> <span>About</span></a>
+                <a href="./resume.php"><span>Resume</span></a></li>
+                <a href="./contact.php"><span>Contact</span></a></li>
+                </ul>
+            </div>
+
+        </div>
