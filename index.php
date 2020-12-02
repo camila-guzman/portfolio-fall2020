9c63889df0196a54fbb7b1d5d049430b16f481da
cg-0 <70660599+cg-0@users.noreply.github.com>
2020-12-02 18:11:44 -0500

    Fix urls

--- index.php
+++ index.php
@@ -8,25 +8,24 @@
   <div class="about-grid">
     <h2>My name is Camila Guzman and I am a front-end developer studying web development.</h2>
     <p>I'm currently a junior at the University of Central Florida, graduating in May 2022. I work at UCF's Design Group as a web designer where I focus on designing and building responsive UX/UI.</p>
-    <p></p>
   </div>
 </div>
 
 <!--portfolio section-->
 <div id="portfolio" class="section">
-  <h1 class="title">Portfolio</h1>
+  <h1 class="title">Portfolio.</h1>
 
   <div class="project">
     <h2>TrunkSide</h2>
     <div class="project-2-grid">
       <img src="./images/trunkside.jpg" />
-      <p class="date">October 2020.</p>
+      <p class="date">October 2020</p>
       <div class="desc">
         <h4 style="margin-bottom: 0; margin-top: 0">TrunkSide is Orlando's first curbside app!</h4>
         <br> I worked as the Lead Designer responsible for strategizing the UX/UI design for the first front-end demo of TrunkSide during Knight Hacks, University of Central Florida's 36 Hour Hackathon. <br><br> I designed prototypes for three screens using Figma. This Hackathon was the first time I had used Figma on a large scale project. I also pair programmed with two of my teammates as an observer, allowing me to use my knowledge of React and CSS to learn the basics of Flutter and lead the UI/UX strategy.<br><br>
         I will continue working with TrunkSide as Lead Designer as it grows into an MVP to serve local customers and restaurants.
       </div>
-      <a class="live-link" href="https://devpost.com/software/trunkside?ref_content=user-portfolio&ref_feature=in_progress">View Project.</a>
+      <a class="live-link" href="https://devpost.com/software/trunkside?ref_content=user-portfolio&ref_feature=in_progress">View Project</a>
 
     </div>
   </div>
@@ -37,13 +36,13 @@
     <h2>Butterscotch Bakery</h2>
     <div class="project-1-grid">
       <img src="./images/butterscotch-bakery-screen.png" />
-      <p class="date">October 2020.</p>
+      <p class="date">October 2020</p>
       <div class="desc"> I created a website for a ficiticious bakery, inspired by my love for baked goods. It was built with PHP, HTML, CSS, and SASS. I used PHP and SASS to create efficient pages with custom styles. <br><br>
         I've started working on a feature built with PHP and Javascript that will allow users to create an account, log in, and place an order. <br><br>
         If you'd like to see this feature in progress, click 'View Code' to view the GitHub repository!
       </div>
-      <a class="live-link" href="https://butterscotch-bakery.herokuapp.com/">View Project.</a>
-      <a class="code-link" href="https://github.com/camila-guzman/butterscotch-bakery">View Code.</a>
+      <a class="live-link" href="https://butterscotch-bakery.herokuapp.com/">View Project</a>
+      <a class="code-link" href="https://github.com/camila-guzman/butterscotch-bakery">View Code</a>
 
     </div>
   </div>
@@ -54,10 +53,10 @@
     <h2>UCF Agencies</h2>
     <div class="project-3-grid">
       <img src="./images/agencies-screen-crop.jpg" />
-      <p class=" date">September 2020.</p>
+      <p class=" date">September 2020</p>
       <div class="desc">This is the revamped page for the University of Central Florida's Office of Student Involvement (OSI) Agencies page that I designed. I created a custom animation that displays each student agency's logo, a short description, and a link to their page. <br><br> This design works for both web and mobile users, providing more context and identity to each agency with a single hover! </div>
-      <a class="live-link" href="https://osi.ucf.edu/agencies/">View Project.</a>
-      <a class="code-link" href="https://osi.ucf.edu/agencies/">View OSI Repository.</a>
+      <a class="live-link" href="https://osi.ucf.edu/agencies/">View Project</a>
+      <a class="code-link" href="https://osi.ucf.edu/agencies/">View OSI Repository</a>
 
     </div>
   </div>
@@ -70,10 +69,10 @@
       <img src="./images/stars-tour.png" />
       <p class="date">July 2020.</p>
       <p class="desc">This is a concept mock-up for the University of Central Florida's Historical Buildings Tour. My design concept centered on making the university archives page a more user-friendly experience for researchers <i>and</i> casual visitors alike, while following UCF's brand standards for the web.<br><br> The mock-up includes eight pages, a home page, a building page, and six pages for each of the building's archived photographs. <br><br> All eight pages were built with HTML, CSS, and UCF's front-end framework based on Bootstrap called 'Athena'. I created seven prototypes with Adobe XD to plan out the layout for mobile, medium, and large screen sizes to plan out a responsive and cohesive design between pages.<br><br></p>
-      <a class="live-link" href="https://camila-guzman.github.io/historical-buildings-tour-ucf/"> View Home Page.</a>
-      <a class="live-link2" href="https://camila-guzman.github.io/historical-buildings-tour-ucf/"> View Building Page.</a>
+      <a class="live-link" href="https://camila-guzman.github.io/historical-buildings-tour-ucf/"> View Home Page</a>
+      <a class="live-link2" href="https://camila-guzman.github.io/historical-buildings-tour-ucf/"> View Building Page</a>
 
-      <a class="code-link" href="https://github.com/camila-guzman/camila-guzman.github.io">View Code.</a>
+      <a class="code-link" href="https://github.com/camila-guzman/camila-guzman.github.io">View Code</a>
 
     </div>
   </div>
@@ -89,17 +88,17 @@
       Download Resume.</a>
 
     <div class="resume-content">
-      <h2 class="resume-title">Education.</h2>
+      <h2 class="resume-title">Education</h2>
       <p>University of Central Florida Orlando, Florida</p>
       <p>Bachelor of Arts in Digital Media, Expected Graduation Date: May 2022</p>
       <p>
         Concentration in Web & Social Platforms, Minor in Computer Science, 3.9 G.P.A
       </p>
 
-      <h2 class="resume-title">Honors & Awards.</h2>
+      <h2 class="resume-title">Honors & Awards</h2>
       <p>UCF Pegasus Gold: Academic Merit Scholarship 2018-2022, Dean’s List for Fall 2018, President’s Honor Roll for Spring 2019, Fall 2019, and Spring 2020</p>
 
-      <h2 class="resume-title">Work Experience.</h2>
+      <h2 class="resume-title">Work Experience</h2>
       <h3 style="margin-bottom:0">UCF Design Group – Web Designer </h3><span>August 2020-Present Orlando, FL/Remote </span>
       <ul>
         <li>Design and manage over 150 pages for the UCF Office of Student Involvement and Student Government using WordPress, HTML, CSS, Bootstrap 4 </li>
@@ -107,7 +106,7 @@
         <li>Create wireframes and prototypes for user interactions using Figma and Adobe XD</li>
       </ul>
 
-      <h2 class="resume-title">Projects.</h2>
+      <h2 class="resume-title">Projects</h2>
       <h3 style="margin-bottom: 0">Knight Hacks Hackathon: TrunkSide – Lead Designer</h3><span>October 2020 Remote</span>
       <ul>
         <li>Led the UX/UI design strategy for mobile application designed to simplify curbside pick-up for Orlando customers and restaurants</li>
@@ -121,19 +120,19 @@
         <li>Currently working on log in features that will allow users to create an account and place an order with PHP and JavaScript</li>
       </ul>
 
-      <h2 class="resume-title">Activities.</h2>
+      <h2 class="resume-title">Activities</h2>
       <h3>Member of UCF Association for Computing Machinery ACM-W Chapter</h3>
       <ul>
         <li>Mentee in Mentorship Program dedicated to empowering women in technical fields and member of Web & Mobile Development Committee</li>
       </ul>
 
-      <h2 class="resume-title">Skills.</h2>
+      <h2 class="resume-title">Skills</h2>
       <h3 style="margin-bottom:0">Languages</h3>
       <p style="margin-top:0">HTML, CSS, JavaScript, SASS, PHP, C, Java</p>
       <h3 style="margin-bottom:0">Frameworks & Libraries</h3>
       <p style="margin-top:0">Bootstrap, React, jQuery</p>
       <h3 style="margin-bottom:0">Tools</h3>
-      <p style="margin-top:0">Node.js, Git, GitHub, Figma, Adobe Suites, Microsoft Office</p>
+      <p style="margin-top:0">Node.js, MySQL, Git, GitHub, WordPress, Adobe Suites, Figma, Microsoft Office</p>
 
     </div>
   </div>
@@ -147,7 +146,7 @@
 
     <ul class="contact-nav">
       <li><a href="mailto:camilamguzman11@gmail.com"><span><i style="margin-right: 10px" class="fab fa-github"></i></span>Email me at camilamguzman11@gmail.com</a></li>
-      <li><a class="linkedin" href="https://www.linkedin.com/in/camila-m-guzman/"><span><i style="margin-right: 10px" class="fab fa-linkedin-in"></i></span>Message me on LinkedIn</a>
+      <li><a class="linkedin" href="https://www.linkedin.com/in/camila-guzman11/"><span><i style="margin-right: 10px" class="fab fa-linkedin-in"></i></span>Message me on LinkedIn</a>
       </li>
     </ul>
 
