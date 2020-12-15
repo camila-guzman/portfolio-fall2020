f982d39df4e677042be9193fe427c52c4905c08f
cg-0 <70660599+cg-0@users.noreply.github.com>
2020-12-15 14:52:52 -0500

    Updating portfolio

--- index.php
+++ index.php
@@ -2,115 +2,133 @@
 <html>
 <?php include './templates/header.php'; ?>
 
-<!--about section-->
-<div id="about" class="section">
-  <h1 class="title">About</h1>
-  <div class="about-grid">
-    <h2>Hello!</h2>
-    <p>My name is Camila Guzman and I am a developer and designer from South Florida. I'm currently a junior at the University of Central Florida, graduating in May 2022. I work at UCF's Design Group as a web designer where I focus on designing and building responsive UX/UI.</p>
+<div class="content">
+  <!--about section-->
+  <div id="about" class="section">
+    <h1 class="title">About</h1>
+    <div class="about-grid">
+      <h2>Hello!</h2>
+      <p>My name is Camila Guzman and I am a developer and designer! I'm currently a junior at the University of Central Florida, graduating in May 2022. I work at UCF's Design Group as a web designer where I focus on designing and building responsive UX/UI for the web.</p>
 
-    <p>Take a look at my portfolio for my latest work and visit my GitHub page for more! </p>
+      <p>Take a look at my portfolio for my latest work and visit my GitHub page for more! </p>
+    </div>
   </div>
-</div>
 
-<!--portfolio section-->
-<div id="portfolio" class="section">
-  <h1 class="title">Portfolio</h1>
+  <!--portfolio section-->
+  <div id="portfolio" class="section">
+    <h1 class="title">Portfolio</h1>
 
-  <div class="project">
-    <h2>Butterscotch Bakery: Update!</h2>
-    <div class="project-1-grid">
-      <img src="./images/butterscotch-bakery-shopping-screen.png" />
-      <p class="date">December 2020</p>
-      <div class="desc"> I added more features to my bakery website!<br><br>
-        Now you can create an account by visiting 'Place Order', log in, place an item in your cart, delete your account, and finally log out. I used MySQL to organize a database with two tables: one for account information with encrypted passwords and a second table for menu items.<br><br>
+    <div class="project">
+      <h2>Butterscotch Bakery: Update!</h2>
+      <div class="project-1-grid">
+        <img src="./images/butterscotch-bakery-shopping-screen.png" />
+        <p class="date">December 2020</p>
+        <div class="desc"> I added more features to my bakery website!<br><br>
+          Now you can create an account by visiting 'Place Order', log in, place an item in your cart, delete your account, and finally log out. I used MySQL to organize a database with two tables: one for account information with encrypted passwords and a second table for menu items.<br><br>
 
-        I'm currently working on creating a fully functioning shopping cart using PHP and MySQL! If you'd like to see this feature in progress, click 'View Code' to view the GitHub repository!
+          I'm currently working on creating a fully functioning shopping cart using PHP and MySQL! If you'd like to see this feature in progress, click 'View Code' to view the GitHub repository!
+        </div>
+        <a class="live-link" href="https://butterscotch-bakery.herokuapp.com/">View Project</a>
+        <a class="code-link" href="https://github.com/camila-guzman/butterscotch-bakery">View Repo</a>
+
       </div>
-      <a class="live-link" href="https://butterscotch-bakery.herokuapp.com/">View Project</a>
-      <a class="code-link" href="https://github.com/camila-guzman/butterscotch-bakery">View Repo</a>
-
     </div>
-  </div>
 
-  <div class="border"></div>
+    <div class="border"></div>
 
-  <div class="project">
-    <h2>TrunkSide</h2>
-    <div class="project-2-grid">
-      <img src="./images/trunkside.jpg" />
-      <p class="date">October 2020</p>
-      <div class="desc">
-        <h4 style="margin-bottom: 0; margin-top: 0">TrunkSide is Orlando's first curbside app!</h4>
-        <br> I worked as the Lead Designer responsible for strategizing the UX/UI design for the first front-end demo of TrunkSide during Knight Hacks, University of Central Florida's 36 Hour Hackathon. <br><br> I designed prototypes for three screens using Figma. This Hackathon was the first time I had used Figma on a large scale project. I also pair programmed with two of my teammates as an observer, allowing me to use my knowledge of React and CSS to learn the basics of Flutter and lead the UI/UX strategy.<br><br>
-        I will continue working with TrunkSide as Lead Designer as it grows into an MVP to serve local customers and restaurants.
+    <div class="project">
+      <h2>Event Feature for WordPress</h2>
+      <div class="project-1-grid">
+        <p class="date">December 2020</p>
+        <div class="desc"> I designed and built a feature for the Office of Student Involvement (OSI) WordPress Theme! Previously, the web team had to manually enter each event using HTML and CSS on the Event page every week. Now, there will be a section to add new events and the theme will automatically display events in a responsive grid layout and order the events by date and time.<br><br>
+
+          I found that this task always took up more time than anticipated at the start of each week so I'm excited for it to become a built-in feature for the OSI WordPress theme. The feature will be live in 2021!
+
+        </div>
+        <a class="code-link" href="https://github.com/sdescommunications/SDES-Communications-Theme-OSI">View OSI Repo</a>
+
       </div>
-      <a class="live-link" href="https://devpost.com/software/trunkside?ref_content=user-portfolio&ref_feature=in_progress">View Our Hackathon Submission</a>
-
     </div>
-  </div>
 
-  <div class="border"></div>
+    <div class="border"></div>
 
-  <div class="project">
-    <h2>Butterscotch Bakery</h2>
-    <div class="project-3-grid">
-      <img src="./images/butterscotch-bakery-screen.png" />
-      <p class="date">October 2020</p>
-      <div class="desc"> I created a website for a ficiticious bakery, inspired by my love for baked goods. It was built with PHP, HTML, CSS, and SASS. I used PHP and SASS to create efficient pages with custom styles. <br><br>
-        I've started working on a feature built with PHP and Javascript that will allow users to create an account, log in, and place an order. <br><br>
-        If you'd like to see this feature in progress, click 'View Code' to view the GitHub repository!
+    <div class="project">
+      <h2>TrunkSide</h2>
+      <div class="project-2-grid">
+        <img src="./images/trunkside.jpg" />
+        <p class="date">October 2020</p>
+        <div class="desc">
+          <h4 style="margin-bottom: 0; margin-top: 0">TrunkSide is Orlando's first curbside app!</h4>
+          <br> I worked as the Lead Designer responsible for strategizing the UX/UI design for the first front-end demo of TrunkSide during Knight Hacks, University of Central Florida's 36 Hour Hackathon. <br><br> I designed prototypes for three screens using Figma. This Hackathon was the first time I had used Figma on a large scale project. I also pair programmed with two of my teammates as an observer, allowing me to use my knowledge of React and CSS to learn the basics of Flutter and lead the UI/UX strategy.<br><br>
+          I will continue working with TrunkSide as Lead Designer as it grows into an MVP to serve local customers and restaurants.
+        </div>
+        <a class="live-link" href="https://devpost.com/software/trunkside?ref_content=user-portfolio&ref_feature=in_progress">View Our Hackathon Submission</a>
+
       </div>
-      <a class="live-link" href="https://butterscotch-bakery.herokuapp.com/">View Project</a>
-      <a class="code-link" href="https://github.com/camila-guzman/butterscotch-bakery">View Repo</a>
-
     </div>
+
+    <div class="border"></div>
+
+    <div class="project">
+      <h2>Butterscotch Bakery</h2>
+      <div class="project-3-grid">
+        <img src="./images/butterscotch-bakery-screen.png" />
+        <p class="date">October 2020</p>
+        <div class="desc"> I created a website for a ficiticious bakery, inspired by my love for baked goods. It was built with PHP, HTML, CSS, and SASS. I used PHP and SASS to create efficient pages with custom styles. <br><br>
+          I've started working on a feature built with PHP and Javascript that will allow users to create an account, log in, and place an order. <br><br>
+          If you'd like to see this feature in progress, click 'View Code' to view the GitHub repository!
+        </div>
+        <a class="live-link" href="https://butterscotch-bakery.herokuapp.com/">View Project</a>
+        <a class="code-link" href="https://github.com/camila-guzman/butterscotch-bakery">View Repo</a>
+
+      </div>
+    </div>
+
+    <div class="border"></div>
+
+    <div class="project">
+      <h2>UCF Agencies</h2>
+      <div class="project-4-grid">
+        <img src="./images/agencies-screen-crop.jpg" />
+        <p class=" date">September 2020</p>
+        <div class="desc">This is the revamped page for the University of Central Florida's Office of Student Involvement (OSI) Agencies page that I designed. I created a custom animation that displays each student agency's logo, a short description, and a link to their page. <br><br> This design works for both web and mobile users, providing more context and identity to each agency with a single hover! </div>
+        <a class="live-link" href="https://osi.ucf.edu/agencies/">View Project</a>
+        <a class="code-link" href="https://github.com/sdescommunications/SDES-Communications-Theme-OSI">View OSI Repo</a>
+
+      </div>
+    </div>
+
+    <div class="border"></div>
+
+    <div class="project">
+      <h2>UCF Historical Buildings Tour</h2>
+      <div class="project-5-grid">
+        <img src="./images/stars-tour.png" />
+        <p class="date">July 2020.</p>
+        <p class="desc">This is a concept mock-up for the University of Central Florida's Historical Buildings Tour. My design concept centered on making the university archives page a more user-friendly experience for researchers <i>and</i> casual visitors alike, while following UCF's brand standards for the web.<br><br> The mock-up includes eight pages, a home page, a building page, and six pages for each of the building's archived photographs. <br><br> All eight pages were built with HTML, CSS, and UCF's front-end framework based on Bootstrap called 'Athena'. I created seven prototypes with Adobe XD to plan out the layout for mobile, medium, and large screen sizes to plan out a responsive and cohesive design between pages.<br><br></p>
+        <a class="live-link" href="https://camila-guzman.github.io/historical-buildings-tour-ucf/"> View Home Page</a>
+        <a class="live-link2" href="https://camila-guzman.github.io/historical-buildings-tour-ucf/"> View Building Page</a>
+
+        <a class="code-link" href="https://github.com/camila-guzman/camila-guzman.github.io">View Repo</a>
+
+      </div>
+    </div>
+
   </div>
 
-  <div class="border"></div>
+  <!--contact section-->
+  <div id="contact" class="section">
+    <h1 class="title">Contact</h1>
+    <div class="contact-grid">
+      <h3>Send me a message!</h3>
 
-  <div class="project">
-    <h2>UCF Agencies</h2>
-    <div class="project-4-grid">
-      <img src="./images/agencies-screen-crop.jpg" />
-      <p class=" date">September 2020</p>
-      <div class="desc">This is the revamped page for the University of Central Florida's Office of Student Involvement (OSI) Agencies page that I designed. I created a custom animation that displays each student agency's logo, a short description, and a link to their page. <br><br> This design works for both web and mobile users, providing more context and identity to each agency with a single hover! </div>
-      <a class="live-link" href="https://osi.ucf.edu/agencies/">View Project</a>
-      <a class="code-link" href="https://osi.ucf.edu/agencies/">View OSI Repo</a>
-
-    </div>
-  </div>
-
-  <div class="border"></div>
-
-  <div class="project">
-    <h2>UCF Historical Buildings Tour</h2>
-    <div class="project-5-grid">
-      <img src="./images/stars-tour.png" />
-      <p class="date">July 2020.</p>
-      <p class="desc">This is a concept mock-up for the University of Central Florida's Historical Buildings Tour. My design concept centered on making the university archives page a more user-friendly experience for researchers <i>and</i> casual visitors alike, while following UCF's brand standards for the web.<br><br> The mock-up includes eight pages, a home page, a building page, and six pages for each of the building's archived photographs. <br><br> All eight pages were built with HTML, CSS, and UCF's front-end framework based on Bootstrap called 'Athena'. I created seven prototypes with Adobe XD to plan out the layout for mobile, medium, and large screen sizes to plan out a responsive and cohesive design between pages.<br><br></p>
-      <a class="live-link" href="https://camila-guzman.github.io/historical-buildings-tour-ucf/"> View Home Page</a>
-      <a class="live-link2" href="https://camila-guzman.github.io/historical-buildings-tour-ucf/"> View Building Page</a>
-
-      <a class="code-link" href="https://github.com/camila-guzman/camila-guzman.github.io">View Repo</a>
+      <ul class="contact-nav">
+        <li><a href="mailto:camilamguzman11@gmail.com"><span><i style="margin-right: 10px" class="fab fa-github"></i></span>Email me at camilamguzman11@gmail.com</a></li>
+        <li><a class="linkedin" href="https://www.linkedin.com/in/camila-guzman11/"><span><i style="margin-right: 10px" class="fab fa-linkedin-in"></i></span>Message me on LinkedIn</a>
+        </li>
+      </ul>
 
     </div>
-  </div>
-
-</div>
-
-<!--contact section-->
-<div id="contact" class="section">
-  <h1 class="title">Contact</h1>
-  <div class="contact-grid">
-    <h3>Send me a message!</h3>
-
-    <ul class="contact-nav">
-      <li><a href="mailto:camilamguzman11@gmail.com"><span><i style="margin-right: 10px" class="fab fa-github"></i></span>Email me at camilamguzman11@gmail.com</a></li>
-      <li><a class="linkedin" href="https://www.linkedin.com/in/camila-guzman11/"><span><i style="margin-right: 10px" class="fab fa-linkedin-in"></i></span>Message me on LinkedIn</a>
-      </li>
-    </ul>
-
   </div>
 </div>
 </body>
